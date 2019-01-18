b7ac1f11a0efbda5d21cb8e9591f89dd77ee1d4c
grudus <jakgruda@gmail.com>
2019-01-18 16:14:28 +0100

    Read JSON file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,38 @@
+{-# LANGUAGE 
+  OverloadedStrings
+, DeriveGeneric
+#-}
+
 module Main where
 
-import Lib
+import Prelude as P
+import Data.Aeson
+import Data.Maybe
+import Text.Read
+import GHC.Generics
+import qualified Data.ByteString.Lazy as L
 
-main :: IO ()
-main = someFunc
+
+data DupaData = DupaConstruct
+  { dupa :: Int
+  } deriving (Show, Generic)
+
+
+instance FromJSON DupaData
+
+
+jsonFile :: FilePath
+jsonFile = "res/dinners.json"
+
+
+getJSON :: IO L.ByteString
+getJSON = L.readFile jsonFile
+
+
+main ::  IO ()
+main = do
+  d <- (eitherDecode <$> getJSON) :: IO (Either String DupaData)
+  case d of
+    Left err -> putStrLn $ "error: " ++ err
+    Right cs -> do
+      print $ cs
